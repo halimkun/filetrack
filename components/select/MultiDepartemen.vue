81e075484c8fe7f79ae300f37573e12484470172
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-06-11 14:12:23 +0700

    feat: Add MultiDepartemen component for selecting multiple departments
    The MultiDepartemen component allows users to select multiple departments. It includes a search functionality and displays a loading state while fetching the department data. The component emits the selected department IDs when the selection changes.

---
+++ components/select/MultiDepartemen.vue
@@ -0,0 +1,60 @@
+<template>
+  <USelectMenu 
+    v-model="selectedDep" 
+    :loading="loadingDep" 
+    :searchable="search" 
+    placeholder="Departement"
+    option-attribute="nama" 
+    class="w-full" 
+    multiple
+    trailing 
+  />
+</template>
+
+<script lang="ts" setup>
+import type { Departemen, DepartemenList } from '~/types/Departemen';
+
+const emit = defineEmits(['update:selectedDep'])
+const { dep } = defineProps<{
+  dep?: string
+}>()
+
+const accessTokenStore = useAccessTokenStore();
+const config = useRuntimeConfig()
+
+const { API_V2_URL } = config.public
+const { accessToken } = accessTokenStore
+const loadingDep = ref(false)
+const selectedDep = ref<Departemen>({
+  dep_id: dep || '',
+  nama: '',
+  kelompok: '',
+  aktif: null,
+  tele_id: ''
+})
+
+async function search(q: string): Promise<Departemen[]> {
+  loadingDep.value = true;
+
+  try {
+    const response = await $fetch<DepartemenList>(`${API_V2_URL}/departemen/search?select=dep_id,nama&limit=100`, {
+      method: 'POST',
+      headers: { Authorization: `Bearer ${accessToken}` },
+      body: JSON.stringify({
+        search: { value: q },
+      })
+    });
+
+    loadingDep.value = false;
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching pegawai:", error);
+    loadingDep.value = false;
+    return [];
+  }
+}
+
+watch(selectedDep, (value) => {
+  emit('update:selectedDep', value.dep_id);
+});
+</script>
