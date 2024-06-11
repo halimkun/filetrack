5708b570f1f300fb856c3ce75d21b8a60d2b4cc7
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-06-11 14:07:28 +0700

    chore: Update @nuxt/ui to version 2.16.0 and add notification styles

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,11 @@
 export default defineAppConfig({
   ui: {
+    notifications: {
+      position: 'top-0 right-2 bottom-auto',
+    },
+    notification: {
+      background: 'bg-white dark:bg-gray-700',
+    },
     icons: {
       dynamic: true
     }
