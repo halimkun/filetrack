c320820d8a52fabbe007824db2d55306302fb3d7
Faisal Halim <ffaisalhalim@gmail.com>
2024-12-09 08:36:12 +0700

    :hammer: Refactor undangan page: update icons and add download functionality for attendance proof

--- pages/undangan/index.vue
+++ pages/undangan/index.vue
@@ -24,7 +24,7 @@
 </template>
 
 <script lang="ts" setup>
-import type { SuratUndangan, Undangan } from '~/types/Undangan';
+import type { SuratUndangan } from '~/types/Undangan';
 import { logEvent } from '~/utils/firebase'
 
 useHead({
@@ -82,22 +82,38 @@ const columns = [
 
 const menu = (row: any) => [
   [
-    { label: "Detail & Kehadiran", icon: "i-heroicons-eye-20-solid", click: () => { router.push(`/${row.tipe}/${btoa(row.no_surat)}`) } },
-    { label: "Tambah Penerima", icon: "i-heroicons-user-plus-solid", click: () => router.push(buildAddRecipientLink(row)) },
+    { label: "Detail & Kehadiran", icon: "i-tabler-search", click: () => { router.push(`/${row.tipe}/${btoa(row.no_surat)}`) } },
+    { label: "Tambah Penerima", icon: "i-tabler-users-plus", click: () => router.push(buildAddRecipientLink(row)) },
   ], [
-    { label: "QR Kehadiran", icon: "i-heroicons-qr-code", click: () => router.push(`/undangan/${btoa(row.no_surat)}/qr`) },
+    { label: "QR Kehadiran", icon: "i-tabler-qrcode", click: () => router.push(`/undangan/${btoa(row.no_surat)}/qr`) },
+    { label: "Bukti Kehadiwan", icon: "i-tabler-file-download", click: () => downloadBuktiKehadiran(row.no_surat) }
   ], [
     { label: "Buat Notulen", icon: "i-tabler-edit-circle", click: () => router.push(`/berkas/notulen/${btoa(row.no_surat)}/new`) }
   ]
 ]
 
-const buildAddRecipientLink = (row: any)  => {
+const downloadBuktiKehadiran = (noSurat: string) => {
+  if (!accessToken) {
+    toast.add({
+      title: 'Gagal',
+      description: 'Token tidak ditemukan, silahkan login kembali',
+      color: 'red',
+      timeout: 5000,
+    });
+    return;
+  };
+
+  const url = `${API_V2_URL}/undangan/penerima/${btoa(noSurat)}/proof?token=${accessToken}`;
+  window.open(url, '_blank');
+}
+
+const buildAddRecipientLink = (row: any) => {
   switch (row.tipe) {
     case 'surat/internal':
       return `${row.tipe}/${btoa(row.no_surat)}/add/recipient`;
 
     default:
-      const noSurat = row.no_surat.split('/').slice(0,1).join('/');
+      const noSurat = row.no_surat.split('/').slice(0, 1).join('/');
       const tglSurat = row.no_surat.split('/').slice(-1).join('/');
 
       // tgl surat is 030524 make it 2024-05-03
