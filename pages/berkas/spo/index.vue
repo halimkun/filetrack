f8b44630f27c0273c1531f21b90b29efb6990618
Faisal Halim <ffaisalhalim@gmail.com>
2025-05-06 11:22:05 +0700

    :art: style: improve table styling and enhance detail display for SPO

--- pages/berkas/spo/index.vue
+++ pages/berkas/spo/index.vue
@@ -15,7 +15,7 @@
         </UButton>
       </div>
     </template>
-    
+
     <TableSpo 
       :response="data"
       :columns="columns"
@@ -28,79 +28,75 @@
   <UModal v-model="isDetailOpen" :title="'Detail SPO'" :ui="{ width: 'w-full sm:max-w-4xl' }">
     <UCard>
       <table class="w-full detail-spo">
-        <thead class="text-sm text-gray-500 dark:text-gray-400">
-          <tr class="align-middle text-center text-sm text-gray-500 dark:text-gray-400">
-            <td rowspan="2" class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 space-y-8 p-3 w-[200px]">
-              <h4 class="text-lg font-bold text-gray-500 dark:text-gray-400 uppercase leading-5">RSIA Aisyiyah Pekajangan</h4>
+        <thead class="text-sm text-gray-500 dark:text-gray-300">
+          <tr class="align-middle text-center text-sm text-gray-500 dark:text-gray-300">
+            <td rowspan="2" class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-300 space-y-8 p-3 w-[200px]">
+              <h4 class="text-lg font-bold text-gray-500 dark:text-gray-300 uppercase leading-5">RSIA Aisyiyah Pekajangan</h4>
               <img src="/logo.png" alt="Logo RSIA Aisyiyah Pekajangan" class="w-24 h-24 mx-auto mb-2" />
             </td>
-            <td colspan="2" class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 p-3">
-              <h3 class="text-xl text-gray-500 dark:text-gray-400 font-bold">
+            <td colspan="2" class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-300 p-3">
+              <h3 class="text-xl text-gray-500 dark:text-gray-300 font-bold">
                 {{ rowSelected.judul }}
               </h3>
             </td>
           </tr>
-          <tr class="align-middle text-center text-sm text-gray-500 dark:text-gray-400">
-            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 p-3">
+          <tr class="align-middle text-center text-sm text-gray-500 dark:text-gray-300">
+            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-300 p-3">
               <p class="font-bold leading-none">No. Dokumen</p>
               <p>{{ rowSelected.nomor }}</p>
             </td>
-            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 p-3">
+            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-300 p-3">
               <p class="font-bold leading-none">Halaman</p>
               <p>1/1</p>
             </td>
           </tr>
         </thead>
         <tbody>
-          <tr class="align-middle text-center text-sm text-gray-500 dark:text-gray-400">
-            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 p-3">
-              <h4 class="text-lg font-bold text-gray-500 dark:text-gray-400 uppercase leading-5">Standar Prosedur Operasional</h4>
+          <tr class="align-middle text-center text-sm text-gray-500 dark:text-gray-300">
+            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-300 p-3">
+              <h4 class="text-lg font-bold text-gray-500 dark:text-gray-300 uppercase leading-5">Standar Prosedur Operasional</h4>
             </td>
-            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 p-3">
+            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-300 p-3">
               <p class="font-bold">Tanggal Ditetapkan</p>
               <p>{{ new Date(rowSelected.tgl_terbit).toLocaleDateString('id-ID', {
                 year: 'numeric', month: 'long', day: '2-digit' 
               }) }}</p>
             </td>
-            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 p-3">
+            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-300 p-3">
               <p class="font-bold leading-none">Ditetapkan</p>
               <p class="font-bold leading-none">Direktur RSIA Aisyiyah Pekajangan</p>
 
               <div class="mt-20">
-                <p>{{ "............." }}</p>
+                <p class="font-bold">{{ rowSelected?.direktur?.nama ?? "............................................................" }}</p>
               </div>
             </td>
           </tr>
 
-          <tr class="align-middle text-sm text-gray-500 dark:text-gray-400">
-            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 text-center p-3 font-bold">Pengertian</td>
-            <td colspan="2" class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 p-3" v-html="unescapeHtml(rowSelected.pengertian)"></td>
+          <tr class="align-middle text-sm text-gray-500 dark:text-gray-300">
+            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-300 text-center p-3 font-bold">Pengertian</td>
+            <td colspan="2" class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-300 p-3" v-html="unescapeHtml(rowSelected.pengertian)"></td>
           </tr>
-          <tr class="align-middle text-sm text-gray-500 dark:text-gray-400">
-            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 text-center p-3 font-bold">Tujuan</td>
-            <td colspan="2" class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 p-3" v-html="unescapeHtml(rowSelected.tujuan)"></td>
+          <tr class="align-middle text-sm text-gray-500 dark:text-gray-300">
+            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-300 text-center p-3 font-bold">Tujuan</td>
+            <td colspan="2" class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-300 p-3" v-html="unescapeHtml(rowSelected.tujuan)"></td>
           </tr>
-          <tr class="align-middle text-sm text-gray-500 dark:text-gray-400">
-            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 text-center p-3 font-bold">Kebijakan</td>
-            <td colspan="2" class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 p-3" v-html="unescapeHtml(rowSelected.kebijakan)"></td>
+          <tr class="align-middle text-sm text-gray-500 dark:text-gray-300">
+            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-300 text-center p-3 font-bold">Kebijakan</td>
+            <td colspan="2" class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-300 p-3" v-html="unescapeHtml(rowSelected.kebijakan)"></td>
           </tr>
-          <tr class="align-middle text-sm text-gray-500 dark:text-gray-400">
-            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 text-center p-3 font-bold">Prosedur</td>
-            <td colspan="2" class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 p-3" v-html="unescapeHtml(rowSelected.prosedur)"></td>
+          <tr class="align-middle text-sm text-gray-500 dark:text-gray-300">
+            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-300 text-center p-3 font-bold">Prosedur</td>
+            <td colspan="2" class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-300 p-3" v-html="unescapeHtml(rowSelected.prosedur)"></td>
           </tr>
-          <tr class="align-middle text-sm text-gray-500 dark:text-gray-400">
-            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 text-center p-3 font-bold">Unit Terkait</td>
-            <td colspan="2" class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 p-3">
+          <tr class="align-middle text-sm text-gray-500 dark:text-gray-300">
+            <td class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-300 text-center p-3 font-bold">Unit Terkait</td>
+            <td colspan="2" class="border dark:border-gray-700 text-sm text-gray-500 dark:text-gray-300 p-3">
               <template v-if="spoUnits && spoUnits.length">
                 <ol class="list-decimal ml-6">
-                  <li v-for="(item, index) in spoUnits" :key="index" class="!mb-0">
+                  <li v-for="(item, index) in spoUnits" :key="index" class="!mb-0 text-gray-800 dark:text-gray-300">
                     {{ item.unit?.nama || item.unit_id }}
                   </li>
                 </ol>
-              </template>
-
-              <template v-else>
-                <p>Tidak ada unit ditemukan.</p>
               </template>
             </td>
           </tr>
@@ -112,7 +108,6 @@
 
 <script lang="ts" setup>
   import type { ResourcePagination } from '~/types/ApiResponse';
-  import { format } from 'date-fns';
 
   const router = useRouter();
   const runtimeConfig = useRuntimeConfig()
